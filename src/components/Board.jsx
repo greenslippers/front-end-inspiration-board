443968693640a683e435d558decddbfc6ddc1bf7
Lina Martinez <lmcastro3501@gmail.com>
2025-06-24 23:56:24 -0700

    Add state, elements, and functionality for pop-up to Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
+import CardForm from './CardForm';
+import FormPopUp from './FormPopUp';
 import './styles/Board.css';
 
+const cards = [];
+
 const Board = ({ board }) => {
-  const cards = [];
+  const [isPopUpOpen, setIsPopUpOpen] = useState(false);
 
   return (
     <div className="board">
       <h1 className="board__title">{board.title}</h1>
       <p className="board__owner">Owner: {board.owner}</p>
+      <button onClick={() => setIsPopUpOpen(true) }>➕ Add Card</button>
+      <FormPopUp isOpen={isPopUpOpen} onClose={() => setIsPopUpOpen(false)}>
+        <CardForm/>
+      </FormPopUp>
       <div className="board__cards-container">
         {/* Cardlist */}
         <Card cardMessage={'Hello'} likesCounter={10} />
