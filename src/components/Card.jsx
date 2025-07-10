e22ce806658a3ae784789311c72b94796845985c
Lina Martinez <lmcastro3501@gmail.com>
2025-07-09 22:07:36 -0700

    Refactor Card.jsx: update Font Awesome imports and adjust button styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
 import PropTypes from 'prop-types';
 import './styles/Card.css'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faTrashCan} from '@fortawesome/free-solid-svg-icons';
 import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const Card = ({id, cardMessage, likesCounter, cardColor, onDeleteCard, onLikeCard }) => {
 	return (
 		<li className="card-item" style={{"--card-bg": cardColor}}>
 			<p className="card-item__message">{cardMessage}</p>
 			<div className="card-item__controls">
-				<button onClick={()=> onLikeCard(id)}><FontAwesomeIcon icon={faThumbsUp} style={{color: "#080808",}} /> 1</button>
+				<button onClick={()=> onLikeCard(id)}><FontAwesomeIcon icon={faThumbsUp} style={{color: "#3a3a3a",}} /> 1</button>
 				<p>{likesCounter} <FontAwesomeIcon icon={faHeart} size="lg" style={{color: "#c40e17",}} /></p>
-				<button onClick={() => onDeleteCard(id)}><FontAwesomeIcon icon={faTrash} style={{color: "#161717",}} /></button>
+				<button onClick={() => onDeleteCard(id)}><FontAwesomeIcon icon={faTrashCan} style={{color: "#3a3a3a",}} /></button>
 			</div>
 		</li>
 	)
