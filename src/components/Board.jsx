7fa6154f139e6ee5b3b987c29219ad6fc79fbe87
Lina Martinez <lmcastro3501@gmail.com>
2025-07-09 18:24:36 -0700

    Refactor Board.jsx: remove local state for popup and pass props for popup control

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
+
 import CardList from './CardList';
 import CardForm from './CardForm';
 import FormPopUp from './FormPopUp';
+
 import './styles/Board.css';
 
-const Board = ({ board, cards, onCreateCard, onDeleteCard, onLikeCard, onSortCards, sortCardsBy }) => {
-  const [isPopUpOpen, setIsPopUpOpen] = useState(false);
-  
-
+const Board = ({
+  board,
+  cards,
+  onCreateCard,
+  onDeleteCard,
+  onLikeCard,
+  onSortCards,
+  sortCardsBy,
+  isPopUpOpen,
+  setIsPopUpOpen
+}) => {
   const handleCreateCard = (boardId, cardData) => {
-    onCreateCard(boardId, cardData)
-    setIsPopUpOpen(false)
-  }
+    onCreateCard(boardId, cardData);
+  };
 
   return (
     <div className="board">
@@ -20,10 +27,19 @@ const Board = ({ board, cards, onCreateCard, onDeleteCard, onLikeCard, onSortCar
         <div className="board__header-left">
           <h1 className="board__title">{board.title}</h1>
         </div>
+
         <div className="board__header-right">
-          <p className="board__owner">Board made by <strong>{board.owner}</strong></p>
-          <button onClick={() => setIsPopUpOpen(true) }>+ Add new card</button>
-          <select className="board__sort-select" value={sortCardsBy} onChange={event => onSortCards(event.target.value)}>
+          <p className="board__owner">
+            Board made by <strong>{board.owner}</strong>
+          </p>
+          <button onClick={() => setIsPopUpOpen(true)}>
+            + Add new card
+          </button>
+          <select
+            className="board__sort-select"
+            value={sortCardsBy}
+            onChange={(event) => onSortCards(event.target.value)}
+          >
             <option value="id">Sort by ID</option>
             <option value="alphabetical">Sort Alphabetically</option>
             <option value="likes">Sort by Likes</option>
@@ -34,9 +50,13 @@ const Board = ({ board, cards, onCreateCard, onDeleteCard, onLikeCard, onSortCar
       <FormPopUp isOpen={isPopUpOpen} onClose={() => setIsPopUpOpen(false)}>
         <CardForm onCreateCard={handleCreateCard} boardId={board.id} />
       </FormPopUp>
-      
+
       <div className="board__cards-container">
-        <CardList cards={cards} onDeleteCard={onDeleteCard} onLikeCard={onLikeCard}/>
+        <CardList
+          cards={cards}
+          onDeleteCard={onDeleteCard}
+          onLikeCard={onLikeCard}
+        />
       </div>
     </div>
   );
@@ -48,11 +68,14 @@ Board.propTypes = {
     title: PropTypes.string.isRequired,
     owner: PropTypes.string.isRequired,
   }).isRequired,
+  cards: PropTypes.array.isRequired,
   onCreateCard: PropTypes.func.isRequired,
   onDeleteCard: PropTypes.func.isRequired,
   onLikeCard: PropTypes.func.isRequired,
   onSortCards: PropTypes.func.isRequired,
   sortCardsBy: PropTypes.string.isRequired,
+  isPopUpOpen: PropTypes.bool.isRequired,
+  setIsPopUpOpen: PropTypes.func.isRequired,
 };
 
 export default Board;
